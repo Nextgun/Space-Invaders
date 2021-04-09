0f2625b466bd8073185e4d44a94a2a5c426a9412
Hector Marrero <hmmarrero38@gmail.com>
2021-04-09 15:11:07 -0500

    started writing and organizing

---
+++ Final Project/Object_Oriented_Game/OOP_Game.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+import math
+from pygame import mixer
+
+
+def draw():
+    pygame.init()   # Initialize the pygame
+    screen = pygame.display.set_mode((800, 600))    # create the screen
---
+++ Final Project/Object_Oriented_Game/enemy.py
@@ -0,0 +1,10 @@
+#object oriented programming
+class enemy(object):
+    def __init__(self, health, *args,  **kwargs):
+        health = 1
+
+    return super().__init__(*args, **kwargs)
+    def thecollosion:
+        #collision code
+        pass
+
